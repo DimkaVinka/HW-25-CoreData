d12e309f25d72bbca750fe425c3d381854d281ee
DimkaVinka <dimkavinka@gmail.com>
2022-04-28 09:46:14 +0300

    Initial Commit

---
+++ HW-25 CoreData/HW_25_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_25_CoreDataApp.swift
+//  HW-25 CoreData
+//
+//  Created by Дмитрий Виноградов on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_25_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
